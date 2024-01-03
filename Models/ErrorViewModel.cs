60ac295603ab01d5a703886ff3dc8cb9bc618605
Salim Karaoğlu <96123185+SalimKaraoglu@users.noreply.github.com>
2024-01-03 08:42:39 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RazorView_Engine.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
